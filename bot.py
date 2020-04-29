b7c42f8136c6199f1ad7f79bbce9090a88289ddd
yccgabby <59523545+yccgabby@users.noreply.github.com>
2020-04-29 13:08:13 -0400

    fix login + add optional notif negate

--- bot.py
+++ bot.py
@@ -5,14 +5,18 @@ from time import sleep, strftime
 from random import randint
 import pandas as pd
 
+# if you want chrome window to run in the background, set as true; otherwise, set as false
+headless = True
+
 # Location of chromedriver, which helps navigate to web pages for automated testing of webapps
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
-chromedriver_path = "/Users/yccgabby/Desktop/chromedriver" # copy and paste with your local path to chromedriver
+if headless:
+    options.add_argument('headless')
+chromedriver_path = "/Users/yccgabby/Desktop/chromedriver"  # copy and paste with your local path to chromedriver
 driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
-sleep(3)
+sleep(1)
 driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
-sleep(3)
+sleep(1)
 
 # gather keys needed to login from terminal
 username = driver.find_element_by_name('username')
@@ -21,17 +25,20 @@ user = input('Please enter your username: ')
 pswd = getpass.getpass('Please enter your password (input will be hidden): ')
 username.send_keys(user)
 password.send_keys(pswd)
-
-# discover the login button with above credentials to login
-login_button = driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div')
-login_button.click()
 sleep(3)
 
-# optional step to navigate away from popup asking about notifications (better solution is to disable popups on chrome)
-notif = driver.find_element_by_css_selector('body > div:nth-child(13) > div > div > div > div.mt3GC > '
-                                            'button.aOOlW.HoLwm')
-notif.click()
+# discover the login button with above credentials to login
+div = driver.find_element_by_css_selector('#react-root > section > main > div')
+buttons = div.find_elements_by_css_selector('button')
+sleep(1)
+buttons[1].click()
+sleep(3)
 
-
-
-
+# optional git step to navigate away from popup asking about notifications
+if not headless:
+    notif_div = driver.find_elements_by_css_selector('body > div')
+    sleep(2)
+    print(len(notif_div))
+    sleep(5)
+    notif_buttons = notif_div[3].find_elements_by_css_selector('button')
+    notif_buttons[1].click()
