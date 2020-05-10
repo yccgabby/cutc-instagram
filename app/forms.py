d7c938bc50a9b7dbdf9961bb4a678c23056e144e
Gabrielle Chan <yccgabby@Gabrielles-MacBook-Pro.local>
2020-05-09 22:52:11 -0400

    add instagram login verification (works!)

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
 from wtforms.validators import DataRequired
+from instapy import InstaPy
+from time import sleep
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+    
+    print(str(password))
+    def validate_password(self, field):
+        try:
+            print(self.username.data, self.password.data)
+            session = InstaPy(username=str(self.username.data),password=str(self.password.data),headless_browser=True)
+            sleep(3)
+            return True
+        except:
+            raise ValidationError('Instagram account does not exist')
